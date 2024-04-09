32f9b12fba66ed82f75a0c8c27991d116acc043f
guicarvalhor <carvalho.guilhermer@gmail.com>
2024-04-09 10:55:23 -0300

    contador 4

--- api/atualizar-contador.js
+++ api/atualizar-contador.js
@@ -1,9 +1,12 @@
 module.exports = async (req, res) => {
-  try {
-    let contador = parseInt(process.env.COUNTER_VALUE || 0);
-    res.status(200).send({ contador });
-  } catch (error) {
-    console.error('Erro ao obter o contador:', error);
-    res.status(500).send('Erro ao obter o contador');
-  }
-};
+    try {
+      let contador = parseInt(process.env.COUNTER_VALUE || 0);
+      contador++;
+      process.env.COUNTER_VALUE = contador.toString();
+      res.status(200).send({ contador });
+    } catch (error) {
+      console.error('Erro ao atualizar o contador:', error);
+      res.status(500).send('Erro ao atualizar o contador');
+    }
+  };
+  
---
+++ api/obter-contador.js
@@ -0,0 +1,10 @@
+module.exports = async (req, res) => {
+    try {
+      let contador = parseInt(process.env.COUNTER_VALUE || 0);
+      res.status(200).send({ contador });
+    } catch (error) {
+      console.error('Erro ao obter o contador:', error);
+      res.status(500).send('Erro ao obter o contador');
+    }
+  };
+  
