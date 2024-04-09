84c935db7d03bc760cabfc5f614d9da7c9f16572
guicarvalhor <carvalho.guilhermer@gmail.com>
2024-04-09 10:47:12 -0300

    teste de incremento

--- api/atualizar-contador.js
+++ api/atualizar-contador.js
@@ -1,12 +1,11 @@
 module.exports = async (req, res) => {
-    try {
-      let contador = parseInt(process.env.COUNTER_VALUE || 0);
-      contador++;
-      process.env.COUNTER_VALUE = contador.toString();
-      res.status(200).send({ contador });
-    } catch (error) {
-      console.error('Erro ao atualizar o contador:', error);
-      res.status(500).send('Erro ao atualizar o contador');
-    }
-  };
-  
+  try {
+    let contador = parseInt(process.env.COUNTER_VALUE || 0);
+    contador++;
+    process.env.COUNTER_VALUE = contador.toString();
+    res.status(200).send({ contador });
+  } catch (error) {
+    console.error('Erro ao atualizar o contador:', error);
+    res.status(500).send('Erro ao atualizar o contador');
+  }
+};
